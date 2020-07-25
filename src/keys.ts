d6da4c95433077a5e07f9e6783b91d673c45fb59
Bartosz Leoniak <leoniak.bartosz@gmail.com>
2020-07-25 22:00:46 +0200

    fix: narrow types (fixes potential bugs)

--- src/keys.ts
+++ src/keys.ts
@@ -1,11 +1,21 @@
-export type Key<T> = keyof T;
-export type ValueOf<T> = T[keyof T];
-export type KnownKeys<T> = {
+import { IsFullyWritable } from './util';
+import { PlainObject } from './types';
+
+export type Key<T extends PlainObject> = keyof T;
+export type ValueOf<T extends PlainObject> = T[keyof T];
+export type KnownKeys<T extends PlainObject> = {
   [K in keyof T]: string extends K ? never : number extends K ? never : K;
 } extends { [_ in keyof T]: infer U }
   ? U
   : never;
-export type OptionalKeys<T> = {
+export type OptionalKeys<T extends PlainObject> = {
   [K in keyof T]-?: undefined extends { [K2 in keyof T]: K2 }[K] ? K : never;
 }[keyof T];
-export type RequiredKeys<T> = Exclude<keyof T, OptionalKeys<T>>;
+export type ReadonlyKeys<T extends PlainObject> = {
+  [P in keyof T]-?: IsFullyWritable<Pick<T, P>> extends true ? never : P;
+}[keyof T];
+export type WritableKeys<T extends PlainObject> = {
+  [P in keyof T]-?: IsFullyWritable<Pick<T, P>> extends true ? P : never;
+}[keyof T];
+export type RequiredKeys<T extends PlainObject> = Exclude<keyof T, OptionalKeys<T>>;
+export type NonNeverKeys<T extends PlainObject> = Pick<T, { [K in keyof T]: T[K] extends never ? never : K }[keyof T]>;

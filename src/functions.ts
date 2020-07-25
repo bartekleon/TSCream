6c162cb01f4944fbbcf1237437d31b239f7932c7
Bartosz Leoniak <leoniak.bartosz@gmail.com>
2020-07-25 21:59:29 +0200

    feat: add deepFreeze function

--- src/functions.ts
+++ src/functions.ts
@@ -1,6 +1,7 @@
 import { PlainObject } from './types';
 import { Key, KnownKeys } from './keys';
 import { Spread } from './util';
+import { DeepReadonly } from './modifyTypes';
 
 export const setItem = <T, K extends keyof T>(object: T, property: K, value: T[K]) => {
   object[property] = value;
@@ -24,4 +25,32 @@ export const extendObject = <T extends PlainObject, U extends PlainObject>(first
 
 export function notEmpty<T>(item: T | undefined | null): item is T {
   return item !== undefined && item !== null;
+}
+
+export function deepFreeze<T>(target: T): DeepReadonly<T> {
+  if (typeof target === 'object') {
+    if (!Object.isFrozen(target)) {
+      Object.freeze(target);
+    }
+    if (target instanceof Map) {
+      target.forEach((v, k) => {
+        deepFreeze(v);
+        deepFreeze(k);
+      });
+    } else if (target instanceof Set) {
+      target.forEach(e => deepFreeze(e));
+    } else {
+      for (const propKey in target) {
+        const desc = Object.getOwnPropertyDescriptor(target, propKey);
+        if (!desc || desc.get || desc.set) {
+          continue;
+        }
+        deepFreeze(desc.value);
+      }
+    }
+
+    return target as DeepReadonly<T>;
+  }
+
+  return (Object.isFrozen(target) ? target : Object.freeze(target)) as DeepReadonly<T>;
 }

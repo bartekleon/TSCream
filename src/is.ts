c375fa945c31a9f09c2bbef785095907b2017d70
Bartosz Leoniak <leoniak.bartosz@gmail.com>
2020-07-25 21:59:50 +0200

    feat: add IsArray, IsObject, IsNonEmptyObject

--- src/is.ts
+++ src/is.ts
@@ -2,3 +2,6 @@ import { Primitive } from './types';
 
 export type IsPrimitive<T> = T extends Primitive ? T : never;
 export type IsTuple<T> = T extends [T] ? T : T extends any[] ? T : never;
+export type IsArray<T> = T extends any[] ? T : never;
+export type IsObject<T> = T extends {} ? T : never;
+export type IsNonEmptyObject<T extends {}> = keyof T extends never ? T : never;
